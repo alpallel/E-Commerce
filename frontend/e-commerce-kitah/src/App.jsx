7fe25551f5cf9d49d4e81323c74630a5a9c7ba96
Alfarrel Ersya <alpallel.e@gmail.com>
2025-11-28 11:56:50 +0700

    add more requirements.txt

--- frontend/e-commerce-kitah/src/App.jsx
+++ frontend/e-commerce-kitah/src/App.jsx
@@ -4,8 +4,8 @@ function App() {
   return (
     <>
       <div class="base" className="pt-16 bg-sky-600 h-screen flex flex-col justify-center items-center w-full">
-        <h2 className="font-bold text-7xl text-white w-3xl text-center">Data to enrich your online business</h2>
-        <p className="text-amber-50 w-xl text-center">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat.</p>
+        <h2 className="font-bold text-7xl text-white w-3xl text-center">Welcome to Tokopetei</h2>
+        <p className="text-amber-50 w-xl text-center">Enjoy peak shopping experience</p>
       </div>
     </>
   );
--- frontend/e-commerce-kitah/src/Footer.jsx
+++ frontend/e-commerce-kitah/src/Footer.jsx
@@ -3,13 +3,11 @@ function Footer() {
     <>
       <div className="h-[50vh] w-full bg-sky-900 text-white px-15 py-12">
         <div class='moreAbout'>
-            <h3 className="font-bold mb-2">More About Tokopetei</h3>
-            <h4>Who we are</h4>
-            <h4>What we do</h4>
-            <h4>Our values</h4>
-            <h4>Leadership</h4>
-            <h4>Our major projects</h4>
-            <h4>The Tokopetei brand</h4>
+            <h3 className="font-bold mb-2">Bulit by Skilled Programmers</h3>
+            <h4>Alfarrel</h4>
+            <h4>Farrel</h4>
+            <h4>Gathfaan</h4>
+            <h4>Umar</h4>
         </div>
       </div>
     </>
